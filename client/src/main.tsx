d1118412d5e5e8f05e04dc93e45659edc64cdff0
robinweitzel <robin.weitzel.rw@gmail.com>
2026-05-28 07:48:38 +0200

    feat(ui): design tokens loaded ahead of styles cascade

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,5 +1,6 @@
 import { createRoot } from "react-dom/client";
 import { App } from "./App";
+import "./ui/tokens.css";
 import "./ui/styles.css";
 
 const rootEl = document.getElementById("root")!;
