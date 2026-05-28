fd3fca8a7a439e23d19b06e4c6a435e1f9a583aa
robinweitzel <robin.weitzel.rw@gmail.com>
2026-05-28 08:13:03 +0200

    feat(session): new stylesheet for chromeless canvas + peek sheet

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -5,6 +5,7 @@ import "./ui/primitives/primitives.css";
 import "./ui/styles.css";
 import "./ui/settings.css";
 import "./ui/home.css";
+import "./ui/session.css";
 
 const rootEl = document.getElementById("root")!;
 // No StrictMode: in dev StrictMode double-mounts components, which would
